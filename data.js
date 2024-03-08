05fe24f7378095fb4f8398418b0776c8e6959f0a
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-03-08 17:51:10 +0100

    renderMenu, add Items to ShoppingCart

--- data.js
+++ data.js
@@ -1,23 +1,26 @@
-const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
-        price: 14,
-        emoji: "🍕"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        id: 1
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 12,
-        emoji: "🍺",
-        id: 2
-    }
-]
+export const menuArray = [
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    id: 0,
+    price: 14,
+    emoji: "🍕",
+    image: "pizza.png",
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 1,
+    image: "hamburger-menu.png",
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 12,
+    emoji: "🍺",
+    id: 2,
+    image: "beer.png",
+  },
+];
---
+++ index.js
@@ -0,0 +1,85 @@
+import { menuArray } from "./data.js";
+
+//Render Menu
+function renderMenu(menu) {
+  const menuItems = menu
+    .map((menu) => {
+      return `
+         <div class="menu__item">
+                    <div class="flex">
+                        <img class="menu__icon" src="./images/${menu.image}" alt="hamburger">
+                        <div>
+                            <h1 class="menu__title">${menu.name}</h1>
+                            <p class="menu__ingredients">${menu.ingredients}</p>
+                            <p class="menu__price">${menu.price}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <a class="button_plus" id="menu-button" data-item="${menu.id}"></a>
+                    </div>
+                </div>
+        `;
+    })
+    .join("");
+
+  return menuItems;
+}
+
+document.getElementById("menu").innerHTML = renderMenu(menuArray);
+
+//Make new array for cart
+const selectedItems = [];
+
+const addButtonElements = document.querySelectorAll(".button_plus");
+addButtonElements.forEach((button) => {
+  button.addEventListener("click", function () {
+    const itemId = button.dataset.item;
+    console.log(itemId);
+    const selectedItem = menuArray.find((item) => item.id == itemId);
+    if (selectedItem) {
+      selectedItems.push(selectedItem);
+      //render selectedItems array every time we add a new item
+      document.getElementById("shopping-list").innerHTML =
+        renderSelectedItems(selectedItems);
+      console.log("Selected items:", selectedItems);
+    }
+  });
+});
+
+//render selectedItems array
+function renderSelectedItems(selectedItems) {
+  const selectedItemsHTML = selectedItems
+    .map((item) => {
+      return `
+      <div class="selected-item">
+        <img class="menu__icon" src="./images/${item.image}" alt="${item.name}">
+        <div>
+          <h1 class="menu__title">${item.name}</h1>
+          <p class="menu__ingredients">${item.ingredients}</p>
+          <p class="menu__price">${item.price}</p>
+        </div>
+      </div>
+    `;
+    })
+    .join("");
+
+  return selectedItemsHTML;
+}
+
+document.getElementById("shopping-list").innerHTML =
+  renderSelectedItems(selectedItems);
+
+// const addButtons = document.querySelectorAll(".button_plus");
+// let cart = [];
+
+// addButtons.forEach((btn) => {
+//   btn.addEventListener("click", function () {
+//     menuArray.map((menu) => {
+//       if (menu.id == btn.dataset.item) {
+//         cart.push(menu);
+//         console.log(cart);
+//       }
+//     });
+//     renderShoppingCart(cart);
+//   });
+// });
